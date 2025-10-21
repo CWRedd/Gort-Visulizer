57ea507a462b7503a2e99e7ba987fc39815df96d
Connor Reddington <99690237+CWRedd@users.noreply.github.com>
2025-10-21 15:51:52 -0400

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import {svelte} from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    
+  base: '/Gort-Visulizer/',
     plugins: [svelte()],
     build: {
         rollupOptions: {
